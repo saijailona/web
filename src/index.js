221ca459cbc255d5b9a1d4439f86f74942d1d929
saijailona <saijatemiseva@gmail.com>
2022-01-23 19:21:47 +0200

    kesken

--- src/index.js
+++ src/index.js
@@ -1,58 +1,91 @@
-import LunchMenu from './assets/sodexo-week-example.json';
-// Test
-console.log(LunchMenu);
+import SodexoLunchMenu from './assets/sodexo-week-example.json';
+// console.log(SodexoLunchMenu);
 
-for (let i = 0; i < coursesEn.length; i++) {
-    console.log(coursesEn[i]);
-    document.getElementById("menuE").innerHTML += coursesEn[i] + '<br />';
+const coursesEn = [];
+const coursesFi = [];
+let language = 'fi';
+let currentMenu = coursesFi;
 
-}
+/**
+ * Extract course titles from Sodexo menu JSON object
+ *
+ * @param {string} menu - JSON Menu to be parsed
+ */
 
-for (let f = 0; f < coursesFi.length; f++) {
-    console.log(coursesFi[f]);
-    document.getElementById("menuF").innerHTML += coursesFi[f] + '<br />';
-}
-
-const langButton = document.getElementById("lang");
-const sortButton = document.getElementById("sort");
-const randomButton = document.getElementById("random");
-
-const enDiv = document.getElementById("menuE");
-const fiDiv = document.getElementById("menuF");
-const sortEn = document.getElementById("sortE");
-const sortFi = document.getElementById("sortF");
-
-langButton.onclick = () => {
-if (enDiv.style.display !== "none") {
-    enDiv.style.display = "none";
-    fiDiv.style.display = "block";
-    langButton.innerHTML = "Eng";
-} else {
-    enDiv.style.display = "block";
-    fiDiv.style.display = "none";
-    langButton.innerHTML = "Fin";
-}
-};
-
-coursesEn.sort();
-coursesFi.sort();
-
-sortButton.onclick = () => {
-coursesEn.reverse();
-coursesFi.reverse();
-
-document.getElementById("menuE").innerHTML = coursesEn.join('<br />');
-document.getElementById("menuF").innerHTML = coursesFi.join('<br />');
+const parseSodexoMenu = (menu) => {
+  const courses = Object.values(menu);
+  for (const course of courses) {
+    coursesEn.push(course.title_en);
+    coursesFi.push(course.title_fi);
+  }
 };
 
 
+const renderMenu = () => {
+  const ulElement = document.querySelector('#sodexo');
+  ulElement.innerHTML = '';
+  for (const item of currentMenu) {
+    const listElement = document.createElement('li');
+    listElement.textContent = item;
+    ulElement.appendChild(listElement);
+  }
+};
 
-randomButton.onclick = () => {
-if (enDiv.style.display !== "none") {
-    let randomDish = coursesEn[Math.floor(Math.random() * coursesEn.length)];
-    document.getElementById("randomDish").innerHTML = randomDish;
-} else {
-    let randomDish = coursesFi[Math.floor(Math.random() * coursesFi.length)];
-    document.getElementById("randomDish").innerHTML = randomDish;
-}
-}
+const switchLanguage = () => {
+  if (language === 'fi') {
+    language = 'en';
+    currentMenu = coursesEn;
+  } else {
+    language = 'fi';
+    currentMenu = coursesFi;
+  }
+};
+
+/**
+ * Sort courses alphapetically
+ *
+ * @param {Array} courses menu array
+ * @param {string} order 'asc'/'desc'
+ * @returns {Array} sorted menu
+ */
+
+const sortCourses = (courses, order = 'asc') => {
+  const sortedCourses = courses.sort();
+  if (order === 'desc') {
+    sortedCourses.reverse();
+  }
+  return sortedCourses;
+};
+
+/**
+ * Picks a random dish
+ *
+ * @param {Array} courses menu
+ * @returns {string} random dish
+ */
+const pickARandomCourse = courses => {
+  const randomIndex = Math.floor(Math.random() * courses.length);
+  return courses[randomIndex];
+};
+
+
+const init = () => {
+  parseSodexoMenu(SodexoLunchMenu.courses);
+  renderMenu();
+
+  document.querySelector('#lang').addEventListener('click', () => {
+    switchLanguage();
+    renderMenu();
+  });
+  document.querySelector('#random').addEventListener('click', () => {
+
+    alert(pickARandomCourse(currentMenu));
+
+  });
+  document.querySelector('#sort').addEventListener('click', () => {
+ 
+    currentMenu = sortCourses(currentMenu, 'desc');
+    renderMenu();
+  });
+};
+init();
